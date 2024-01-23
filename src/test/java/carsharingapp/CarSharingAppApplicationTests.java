99f648e33c98f166ac253f8b54fea4fdaeec8935
Vlad Kucher <kuchervlad49@gmail.com>
2024-01-23 21:02:32 +0200

    fix v2

--- src/test/java/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/carsharingapp/CarSharingAppApplicationTests.java
@@ -1,13 +1,8 @@
 package carsharingapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarSharingAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
